03dbab106234d85c10df7ef6bca5e4ec019de765
Luciano Barreira <luciano.barreira@tfgco.com>
2018-09-11 00:52:11 -0300

    Add FSM prototype and Hinge Joint test for ledge grab mechanic

---
+++ Assets/Scripts/Player/FSM.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+
+public class FSM : MonoBehaviour
+{
+    [SerializeField] public List<State> states ;
+    [SerializeField] private int current;
+    private float timer;
+
+    private void Awake()
+    {
+        states = new List<State>();
+        timer = 0f;
+    }
+
+    private void Update()
+    {
+        if (current >= 0 && current < states.Count)
+        {
+            states[current].OnStateUpdate.Invoke();
+            if (timer >= states[current].Duration)
+            {
+                timer = 0f;
+                states[current].OnStateExit.Invoke();
+                current = states[current].NextState;
+                states[current].OnStateEnter.Invoke();
+            }
+            timer += Time.deltaTime;
+        }
+    }
+}
+
+namespace Player
+{
+    public class Walking : State{}
+    public class LedgeIdle : State {}
+    public class LedgeLifting : State {}
+}
+
---
+++ Assets/Scripts/Player/State.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class State
+{
+
+    [SerializeField] private string _name;
+    [SerializeField] private float _stateDuration;
+    [SerializeField] private float _randomness;
+    [SerializeField] private int _nextState;
+
+    public int NextState
+    {
+        set { this._nextState = value; }
+        get { return this._nextState;  }
+    }
+    public float Duration
+    {
+        get { return _stateDuration; }
+    }
+    public UnityEvent OnStateEnter;
+    public UnityEvent OnStateUpdate;
+    public UnityEvent OnStateExit;
+}
